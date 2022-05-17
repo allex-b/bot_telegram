1cc9ac712ef68f05806bd5bebd29752f9793785d
allex-b <80401677+allex-b@users.noreply.github.com>
2022-05-17 08:22:07 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -16,16 +16,13 @@ def index():
 @app.route('/update_server', methods=['POST'])
 def webhook_git():
     if request.method == 'POST':
-        repo = git.Repo('mysite')
+        repo = git.Repo('/var/www/sites/mysite')
         origin = repo.remotes.origin
-        repo.git.checkout("HEAD~1")
-        repo.remotes.origin.pull()
-        origin.git.checkout("master")
-
-
+        #repo.git.checkout("HEAD~1")
+        #repo.remotes.origin.pull()
+        #origin.git.checkout("master")
         origin.pull()
-
-
+        
         return 'Updated PythonAnywhere successfully', 200
     else:
         return 'Wrong event type', 400
